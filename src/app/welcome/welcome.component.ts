32aaf32a251cfc0a7fd3b2eb9a23fb9d436cc6b3
Andreea Racovita <andreearacovitagit@gmail.com>
2023-12-02 13:49:31 +0200

    Add a simple hello world data service using HttpClient

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,21 +2,36 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HelloWorldBean, WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
-
   name: string = '';
-  // ActivatedRoute
-  constructor(private route: ActivatedRoute) { }
+  welcomeMessageFromService: string = '';
+  
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) { }
 
   ngOnInit() {
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMessage(): void {
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response)
+    );
+  }
+
+  handleSuccessfulResponse(response: HelloWorldBean): void {
+    this.welcomeMessageFromService = response.message;
   }
 }
