c70df08f980912016d62bf279e76c1fd09b998d7
Andreea Racovita <andreearacovitagit@gmail.com>
2023-12-20 16:20:21 +0200

    Switch to jpa todo data url

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Todo } from '../../list-todos/list-todos.component';
-import { API_URL } from '../../app.constants';
+import { TODO_JPA_API_URL } from '../../app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +13,22 @@ export class TodoDataService {
   ) { }
 
   retrieveAllTodos(username: string) {
-    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`);
+    return this.http.get<Todo[]>(`${TODO_JPA_API_URL}/users/${username}/todos`);
   }
 
   deleteTodoById(username: string, id: number) {
-    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`);
+    return this.http.delete(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`);
   }
 
   retrieveTodoById(username: string, id: number) {
-    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`);
+    return this.http.get<Todo>(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`);
   }
 
   updateTodoById(username: string, id: number, todo: Todo) {
-    return this.http.put<Todo>(`${API_URL}/users/${username}/todos/${id}`, todo);
+    return this.http.put<Todo>(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`, todo);
   }
 
   addTodo(username: string, todo: Todo) {
-    return this.http.post(`${API_URL}/users/${username}/todos`, todo);
+    return this.http.post(`${TODO_JPA_API_URL}/users/${username}/todos`, todo);
   }
 }
